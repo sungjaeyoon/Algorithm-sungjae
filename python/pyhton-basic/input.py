c0a0398f73a1385865369bf833cc6c91592b6e8f
sungjaeyoon <dbstjdwo0000@gmail.com>
2020-11-10 21:58:01 +0900

    Create input.py

---
+++ python/pyhton-basic/input.py
@@ -0,0 +1,17 @@
+# n = int(input())
+#
+# data = list(map(int, input().split()))
+# a, b, c = map(int, input().split())
+#
+# print(n)
+# print(a,b,c)
+
+
+# import sys
+#
+# data = sys.stdin.readline().rstrip()
+#
+# print(data)
+# print(data, end="sd")
+#
+# print(f"zzzz{data}")

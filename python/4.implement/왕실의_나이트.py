3025553d00f07fdb81e7b7057391f0eeaea3ff24
sungjaeyoon <dbstjdwo0000@gmail.com>
2020-11-12 22:45:19 +0900

    Create 왕실의_나이트.py

---
+++ python/4.implement/왕실의_나이트.py
@@ -0,0 +1,17 @@
+location = input()
+
+x, y = int(ord(location[0])) - 96, int(location[1])
+
+dx = [-2, -1, 1, 2, 2, 1, -1, -2]
+dy = [1, 2, 2, 1, -1, -2, -2, -1]
+
+print(x, y)
+count = 0
+
+for i in range(8):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if 1 <= nx <= 8 and 1 <= ny <= 8:
+        count += 1
+
+print(count)

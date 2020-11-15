8abf6223362a73b0a645072ef186ff350ad96a34
sungjaeyoon <dbstjdwo0000@gmail.com>
2020-11-15 13:45:06 +0900

    Update 효율적인_화페구성.py

--- python/8.다이나믹프로그래밍/효율적인_화페구성.py
+++ python/8.다이나믹프로그래밍/효율적인_화페구성.py
@@ -1,18 +1,18 @@
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 
 arr = []
 for i in range(n):
     arr.append(int(input()))
 
-d = [10001] * (m + 1)
+d = [10001] * (k + 1)
 d[0] = 0
 
-for i in arr:
-    for j in range(arr[i], m + 1):
+for i in range(n):
+    for j in range(arr[i], k + 1):
         if d[j - arr[i]] != 10001:
-            d[j] = min(d[i], d[j - arr[i]] + 1)
+            d[j] = min(d[j], d[j - arr[i]] + 1)
 
-if d[m] == 10001:
+if d[k] == 10001:
     print(-1)
 else:
-    print(d[m])
+    print(d[k])

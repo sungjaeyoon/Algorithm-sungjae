ad30b42a2dcf2f097e99f6d68447c62d8b9f919a
sungjaeyoon <dbstjdwo0000@gmail.com>
2020-11-15 19:20:56 +0900

    Update P9465_스티커.py

--- python/백준-다이나믹프로그래밍/P9465_스티커.py
+++ python/백준-다이나믹프로그래밍/P9465_스티커.py
@@ -10,7 +10,7 @@ for _ in range(t):
     d[1][1] = second_row[0]
 
     for i in range(2, n + 1):
-        d[0][i] = max(max(d[1][i - 1], d[1][i - 2]) + first_row[i - 1], d[0][i - 1])
-        d[1][i] = max(max(d[0][i - 1], d[0][i - 2]) + second_row[i - 1], d[1][i - 1])
+        d[0][i] = max(d[1][i - 1] + first_row[i - 1], d[0][i - 1])
+        d[1][i] = max(d[0][i - 1] + second_row[i - 1], d[1][i - 1])
 
     print(max(d[0][n], d[1][n]))
